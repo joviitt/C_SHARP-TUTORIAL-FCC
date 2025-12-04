d7c3127ee38467e4258f3e229fbd983d1293bc6e
joviitt <jovitjohn2004@gmail.com>
2025-12-04 15:22:07 +0530

    switch statements

--- SwitchStatements/SwitchStatements/Program.cs
+++ SwitchStatements/SwitchStatements/Program.cs
@@ -10,9 +10,33 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.Write("enter  day of the week: ");
+            Console.Write("enter day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("It is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("It is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("It is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("It is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("It is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("It is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("It is Sunday");
+                    break; 
 
-            hellp
+            }
 
 
 
